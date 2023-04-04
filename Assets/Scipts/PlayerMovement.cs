65828a48f0d9cf1b56b7960e4224b6e0a31cb1f8
flowfy <flowfy@gmail.com>
2023-04-04 23:06:56 +0200

    Animations

--- Assets/Scipts/PlayerMovement.cs
+++ Assets/Scipts/PlayerMovement.cs
@@ -1,45 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody rb;
-    public float movementSpeed = 6f;
-    public float jumpForce = 5f;
+    public float speed = 10.0f;
+    public float jumpForce = 500.0f;
+    public float moveThreshold = 0.1f;
+    public Animator animator;
 
-    public Transform groundCheck;
-    public LayerMask ground;
+    private Rigidbody rigidBody;
+    public bool isGrounded = true;
 
-    Animator animCtrl;
-
-    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        animCtrl = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Bewegen
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
-        // Springen
-        if (Input.GetButtonDown("Jump") && isGrounded())
+        if (movement.x > 0)
         {
-            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
         }
-        animCtrl.SetBool("isJumping", && isGrounded());
+        else if (movement.x < 0)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
+        }
 
+        if (Mathf.Abs(movement.x) > 0)
+        {
+            animator.SetBool("isRunning", true);
+            transform.position += new Vector3(movement.x * speed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Debug.Log("Jump pressed");
+            rigidBody.AddForce(new Vector3(0.0f, jumpForce, 0.0f));
+        }
     }
 
-    bool isGrounded()
+    void OnCollisionEnter(Collision collision)
     {
-        return Physics.CheckSphere(groundCheck.position, .1f, ground);
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            animator.SetBool("isJumping", false);
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+            animator.SetBool("isJumping", true);
+        }
     }
 }
